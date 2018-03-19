a5ea479de7e88cc78e398dd568ce3c21d1a6926f
Hoon Lee <yl.hoony@gmail.com>
2018-03-19 12:52:36 -0700

    set NavMenu routes in routes config file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+
+  root to: "nav_menu#index"
+
+  get "dashboard", to: "nav_menu#dashboard"
+  get "demand", to: "nav_menu#demand"
+  get "supply", to: "nav_menu#supply"
+  get "product", to: "nav_menu#product"
+  get "warehouse", to: "nav_menu#warehouse"
+  get "logistics", to: "nav_menu#logistics"
+  get "report", to: "nav_menu#report"
+  get "setup", to: "nav_menu#setup"
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
