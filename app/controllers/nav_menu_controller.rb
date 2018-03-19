ea36bc8849efe129b4dc004536a3bc3e5405cf2c
Hoon Lee <yl.hoony@gmail.com>
2018-03-19 12:45:10 -0700

    added NavMenu route to controller and created view files

--- app/controllers/nav_menu_controller.rb
+++ app/controllers/nav_menu_controller.rb
@@ -1,2 +1,39 @@
 class NavMenuController < ApplicationController
+
+  def index
+
+  end
+
+  def dashboard
+    
+  end
+
+  def demand
+    
+  end
+
+  def supply
+    
+  end
+
+  def product
+    
+  end
+
+  def warehouse
+    
+  end
+
+  def logistics
+    
+  end
+
+  def report
+    
+  end
+
+  def setup
+    
+  end
+
 end
