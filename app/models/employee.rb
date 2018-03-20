bb8f42dd00417d4a8f7375ec96156aa3d1116666
Hoon Lee <yl.hoony@gmail.com>
2018-03-20 10:26:53 -0700

    added validation with scope

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,9 +2,8 @@ class Employee < ApplicationRecord
   belongs_to :company
   belongs_to :user
 
-  # def active_employees
-  #   where(active: true)
-  # end
+  validates :user_id, presence: true
+  validates :company_id, presence: true, uniqueness: { scope: :user_id, message: "exists as employee." }
 
   def full_name
     "#{user.first_name} #{user.last_name}"
