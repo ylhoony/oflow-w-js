618c1a78aa2150f3e4ed08a17224bab3f136eeaa
Hoon Lee <yl.hoony@gmail.com>
2018-03-19 19:41:21 -0700

    set routes and updated migration file

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,2 +1,29 @@
 class CountriesController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  private
+
+    def country_params
+      # params.require(:country).permit(:name)
+    end
+
 end
