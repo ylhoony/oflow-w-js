7a03dc5b27b6e154cc7b1643c78ec16a47d89413
Hoon Lee <yl.hoony@gmail.com>
2018-03-20 11:14:50 -0700

    generated Account resource

---
+++ db/migrate/20180320181318_create_accounts.rb
@@ -0,0 +1,17 @@
+class CreateAccounts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :accounts do |t|
+      t.string :type
+      t.integer :company_id
+      t.string :name
+      t.string :tax_id_number
+      t.text :comment
+      t.integer :payment_term_id
+      t.integer :currency_id
+      t.integer :warehouse_id
+      t.boolean :active, dafault: false
+
+      t.timestamps
+    end
+  end
+end
