6743ce5c3abb5e09a937ac8e8ca7c01fba188c29
Hoon Lee <yl.hoony@gmail.com>
2018-03-20 00:52:12 -0700

    updated currencies index and form, and set controller logic for new and create

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -1,15 +1,21 @@
 class CurrenciesController < ApplicationController
+  before_action :set_currency, only: [:edit, :update]
 
   def index
-    
+    @currencies = Currency.all
   end
 
   def new
-    
+    @currency = Currency.new
   end
 
   def create
-    
+    @currency = Currency.new(currency_params)
+    if @currency.save
+      redirect_to currencies_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -22,8 +28,12 @@ class CurrenciesController < ApplicationController
 
   private
 
+    def set_currency
+      @currency = Currency.find(params[:id])
+    end
+
     def currency_params
-      # params.require(:currency).permit(:name, )
+      params.require(:currency).permit(:name, :code_alpha, :code_numeric, :active)
     end
 
 end
