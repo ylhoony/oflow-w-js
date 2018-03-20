774138a2219725f8f08bb43f36208171907946b0
Hoon Lee <yl.hoony@gmail.com>
2018-03-19 20:46:42 -0700

    updated validation for countries

--- app/models/country.rb
+++ app/models/country.rb
@@ -2,8 +2,8 @@ class Country < ApplicationRecord
   has_many :companies
 
   validates :name, presence: true, uniqueness: true
-  validates :alpha_2_code, presence: true, uniqueness: true
-  validates :alpha_3_code, presence: true, uniqueness: true
-  validates :numeric_code, presence: true, uniqueness: true
+  validates :alpha_2_code, length: { is: 2 }, presence: true, uniqueness: true
+  validates :alpha_3_code, length: { is: 3 }, presence: true, uniqueness: true
+  validates :numeric_code, length: { is: 3 }, presence: true, uniqueness: true
 
 end
