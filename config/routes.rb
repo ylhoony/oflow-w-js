a6e58c21ff21aa31d90daec9524ab5c1cdfd3c63
Hoon Lee <yl.hoony@gmail.com>
2018-03-19 19:38:04 -0700

    generated Country resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root to: "nav_menu#index"
@@ -17,5 +18,6 @@ Rails.application.routes.draw do
   resources :companies
   resources :employees
   resources :currencies, except: [:destroy]
+  resources :countries, except: [:destroy]
   
 end
