f80bed2e71c7ad12424f81a3c7f5fa26edac39b6
Hoon Lee <yl.hoony@gmail.com>
2018-03-20 09:41:28 -0700

    added full_name method

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,13 @@
 class Employee < ApplicationRecord
   belongs_to :company
   belongs_to :user
+
+  # def active_employees
+  #   where(active: true)
+  # end
+
+  def full_name
+    "#{user.first_name} #{user.last_name}"
+  end
   
 end
