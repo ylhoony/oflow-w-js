00eabef035b8e3927ce58125d66599cc1c97aa13
Hoon Lee <yl.hoony@gmail.com>
2018-03-20 10:32:24 -0700

    set redirection on root path

--- app/controllers/nav_menu_controller.rb
+++ app/controllers/nav_menu_controller.rb
@@ -1,7 +1,11 @@
 class NavMenuController < ApplicationController
 
   def index
-
+    if user_signed_in?
+      redirect_to dashboard_path
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   def dashboard
