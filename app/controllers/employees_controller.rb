13f102558d751291be8472725ce82a483893e83a
Hoon Lee <yl.hoony@gmail.com>
2018-03-19 18:43:49 -0700

    migrated Employee file and added routes method to Employees controller

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,33 @@
 class EmployeesController < ApplicationController
+
+  def index
+    
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+    def employee_params
+      # params.require(:employee).permit(:)
+    end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320011415) do
+ActiveRecord::Schema.define(version: 20180320013726) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
     t.integer "company_id"
     t.integer "currency_id"
     t.integer "active", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "employees", force: :cascade do |t|
+    t.integer "company_id"
+    t.integer "user_id"
+    t.string "job_title"
+    t.boolean "admin", default: false
+    t.boolean "active", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
