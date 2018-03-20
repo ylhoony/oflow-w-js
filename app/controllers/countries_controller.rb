5b4c6782023d790c6425d0a84d185a66e32227ac
Hoon Lee <yl.hoony@gmail.com>
2018-03-19 20:46:16 -0700

    set route logics

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,4 +1,5 @@
 class CountriesController < ApplicationController
+  before_action :set_country, only: [:edit, :update]
 
   def index
     @countries = Country.all
@@ -9,21 +10,33 @@ class CountriesController < ApplicationController
   end
 
   def create
-    
+    @country = Country.new(country_params)
+    if @country.save
+      redirect_to countries_path
+    else
+      render :new
+    end
   end
 
   def edit
-    
   end
 
   def update
-    
+    if @country.update(country_params)
+      redirect_to countries_path
+    else
+      render :edit
+    end
   end
 
   private
 
+    def set_country
+      @country = Country.find(params[:id])
+    end
+
     def country_params
-      # params.require(:country).permit(:name)
+      params.require(:country).permit(:name, :alpha_2_code, :alpha_3_code, :numeric_code, :active)
     end
 
 end
