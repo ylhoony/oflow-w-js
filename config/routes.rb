2714b731cdd9d4a8be4b9a4e237f9c94a4a65899
Hoon Lee <yl.hoony@gmail.com>
2018-03-19 19:24:47 -0700

    generated currencies resouce

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-
-  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root to: "nav_menu#index"
@@ -18,5 +16,6 @@ Rails.application.routes.draw do
 
   resources :companies
   resources :employees
+  resources :currencies
   
 end
