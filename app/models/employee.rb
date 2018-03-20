e417fdf218318e2e2274a8a7f55fd68499ad76f8
Hoon Lee <yl.hoony@gmail.com>
2018-03-20 11:10:41 -0700

    moved scope validation to user_id in employees

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,8 +2,8 @@ class Employee < ApplicationRecord
   belongs_to :company
   belongs_to :user
 
-  validates :user_id, presence: true
-  validates :company_id, presence: true, uniqueness: { scope: :user_id, message: "exists as employee." }
+  validates :user_id, presence: true, uniqueness: { scope: :company_id }
+  validates :company_id, presence: true
 
   def full_name
     "#{user.first_name} #{user.last_name}"
