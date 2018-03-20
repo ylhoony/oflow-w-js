6e32450358c64e40500f76829e754adf5deba2e1
Hoon Lee <yl.hoony@gmail.com>
2018-03-20 02:07:16 -0700

    added current_company setter getter methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,12 @@ class User < ApplicationRecord
   has_many :employees
   has_many :companies, through: :employees
 
+  def current_company=(company)
+    self.update(current_company_id: company.id)
+  end
+
+  def current_company
+    Company.find(self.current_company_id) unless current_company_id.nil?
+  end
+
 end
