f9e18432ab57ec522745cb7fabe8e0c34c87a23c
Hoon Lee <yl.hoony@gmail.com>
2018-03-20 19:27:46 -0700

    set routes methods for warehouse

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,2 +1,38 @@
 class WarehousesController < ApplicationController
+
+  def index
+    
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+
+  end
+
+  private
+
+    def warehouse_params
+      # params.require(:warehouse).permit(:)
+    end
+
+
 end
