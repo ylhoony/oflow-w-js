1b35441d763fb1addd2f7c9d9446a41f1912fe1e
Hoon Lee <yl.hoony@gmail.com>
2018-03-21 10:51:42 -0700

    set the company address controller logics

--- app/controllers/company_addresses_controller.rb
+++ app/controllers/company_addresses_controller.rb
@@ -1,2 +1,70 @@
 class CompanyAddressesController < ApplicationController
+  before_action :require_signed_in?
+  before_action :set_countries, except: [:index, :destroy]
+  before_action :set_company_address, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @company_addresses = current_company.company_addresses.all
+  end
+
+  def new
+    @company_address = CompanyAddress.new
+  end
+
+  def create
+    @company_address = CompanyAddress.new(comapny_address_params)
+    if @company_address.save
+      redirect_to company_addresses_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    if @company_address.update(comapny_address_params)
+      redirect_to ompany_addresses_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @company_address.delete
+    redirect_to ompany_addresses_path
+  end
+
+  private
+
+    def set_countries
+      @countries = Country.active_countries
+    end
+
+    def set_company_address
+      @company_address = CompanyAddress.find(params[:id])
+    end
+
+    def company_address_params
+      params.require(:company_address).permit(
+        :company_id,
+        :name,
+        :attention,
+        :address_line_1,
+        :address_line_2,
+        :city,
+        :state,
+        :country_id,
+        :postal_code,
+        :phone,
+        :fax,
+        :active)
+    end
+
 end
