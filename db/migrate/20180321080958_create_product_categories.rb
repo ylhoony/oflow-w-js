0b3f331c2ec6798c7cd8a32cc410a72e3a433c87
Hoon Lee <yl.hoony@gmail.com>
2018-03-21 01:11:37 -0700

    migrated product category

--- db/migrate/20180321080958_create_product_categories.rb
+++ db/migrate/20180321080958_create_product_categories.rb
@@ -1,9 +1,11 @@
 class CreateProductCategories < ActiveRecord::Migration[5.1]
   def change
     create_table :product_categories do |t|
+      t.integer :company_id
       t.string :name
+      t.boolean :active, default: false
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180321080612) do
+ActiveRecord::Schema.define(version: 20180321080958) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "type"
@@ -87,6 +87,14 @@ ActiveRecord::Schema.define(version: 20180321080612) do
     t.integer "trade_credit_days", default: 0
     t.integer "date_days", default: 0
     t.integer "payment_option_id"
+    t.boolean "active", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "product_categories", force: :cascade do |t|
+    t.integer "company_id"
+    t.string "name"
     t.boolean "active", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
