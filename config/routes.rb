fea9a9617e7b1cee4482cd2d1c7f0b6a5d2650d6
Hoon Lee <yl.hoony@gmail.com>
2018-03-21 01:10:21 -0700

    generated Product Category resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :product_categories
   resources :unit_of_measures
   resources :freight_terms
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
