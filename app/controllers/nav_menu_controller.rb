0749c11acfe01304b6b7f71c86d0e385c383bf83
Hoon Lee <yl.hoony@gmail.com>
2018-03-21 10:37:07 -0700

    fixed redirecting route

--- app/controllers/nav_menu_controller.rb
+++ app/controllers/nav_menu_controller.rb
@@ -3,7 +3,7 @@ class NavMenuController < ApplicationController
 
   def index
     if user_signed_in?
-      redirect_to dashboard_path
+      redirect_to nav_dashboard_path
     else
       redirect_to new_user_session_path
     end
