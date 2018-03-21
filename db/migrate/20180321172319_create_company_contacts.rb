fdf9de145db847fcabd6d842a96b4febad561eea
Hoon Lee <yl.hoony@gmail.com>
2018-03-21 10:29:15 -0700

    updated and migrated CompanyContact

--- db/migrate/20180321172319_create_company_contacts.rb
+++ db/migrate/20180321172319_create_company_contacts.rb
@@ -1,9 +1,16 @@
 class CreateCompanyContacts < ActiveRecord::Migration[5.1]
   def change
     create_table :company_contacts do |t|
+      t.integer :company_id
       t.string :first_name
+      t.string :last_name
+      t.string :job_title
+      t.string :email
+      t.string :phone
+      t.text :comment
+      t.boolean :active, default: false
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180321170134) do
+ActiveRecord::Schema.define(version: 20180321172319) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "type"
@@ -48,6 +48,19 @@ ActiveRecord::Schema.define(version: 20180321170134) do
     t.string "postal_code"
     t.string "phone"
     t.string "fax"
+    t.boolean "active", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "company_contacts", force: :cascade do |t|
+    t.integer "company_id"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "job_title"
+    t.string "email"
+    t.string "phone"
+    t.text "comment"
     t.boolean "active", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
