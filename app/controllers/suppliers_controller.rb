deb0705b2bd6ff0885094cf982f499998e81cdd2
Hoon Lee <yl.hoony@gmail.com>
2018-03-20 20:42:44 -0700

    added routes

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,7 +1,37 @@
 class SuppliersController < ApplicationController
 
   def index
+    @suppliers = current_company.suppliers.all
+  end
+
+  def new
     
   end
+
+  def create
+
+  end
+
+  def show
+    
+  end
+
+  def edit
+
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+    def supplier_params
+      
+    end
   
 end
