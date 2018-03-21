e1f20fe3601d24ae76e526a18c58148350004763
Hoon Lee <yl.hoony@gmail.com>
2018-03-20 18:14:43 -0700

    added active_payment_options class method and set validation on payment_term

--- app/models/payment_option.rb
+++ app/models/payment_option.rb
@@ -2,4 +2,8 @@ class PaymentOption < ApplicationRecord
   has_many :payment_terms
 
   validates :name, presence: true, uniqueness: true
+
+  def self.active_payment_options
+    where(active: true)
+  end
 end
--- app/models/payment_term.rb
+++ app/models/payment_term.rb
@@ -1,4 +1,6 @@
 class PaymentTerm < ApplicationRecord
   belongs_to :company
   belongs_to :payment_option
+
+  validates :name, presence: true
 end
