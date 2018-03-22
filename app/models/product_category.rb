c2480b11c81651b24b21b18ded4107df81731c0c
Hoon Lee <yl.hoony@gmail.com>
2018-03-22 15:27:21 -0700

    added validation for name

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -2,5 +2,13 @@ class ProductCategory < ApplicationRecord
   belongs_to :company
   has_many :product_groups
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
+
+  def product_groups_attributes=(product_groups)
+    product_groups.each do |i, product_group|
+      self.product_groups.find_by(name: product_group[:name]) || self.product_groups.build(product_group)
+    end
+    self.save
+  end
+
 end
--- app/models/product_group.rb
+++ app/models/product_group.rb
@@ -1,3 +1,5 @@
 class ProductGroup < ApplicationRecord
   belongs_to :product_category
+
+  validates :name, presence: true, uniqueness: { scope: :product_category_id }
 end
