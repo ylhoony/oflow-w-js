74c45488a3b1dce8089895f9eac0b3b009b78ccb
Hoon Lee <yl.hoony@gmail.com>
2018-03-22 14:45:03 -0700

    updated variables

--- app/controllers/product_groups_controller.rb
+++ app/controllers/product_groups_controller.rb
@@ -1,2 +1,49 @@
 class ProductGroupsController < ApplicationController
+  before_action :require_signed_in?
+  before_action :set_product_group, only: [:edit, :update]
+
+  def index
+    @product_groups = current_company.product_groups.all
+  end
+
+  def new
+    @product_group = ProductGroup.new
+  end
+
+  def create
+    @product_group = ProductGroup.new(product_group_params)
+    if @product_group.save
+      redirect_to product_groups_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @product_group.update(product_group_params)
+      redirect_to product_groups_path
+    else
+      render :new
+    end
+  end
+
+  def switch
+    product_group = ProductGroup.find(params[:id])
+    product_group.active? ? product_group.update(active: false) : product_group.update(active: true)
+    redirect_to product_groups_path, notice: "Status is updated successfully."
+  end
+
+  private
+
+    def set_product_group
+      @product_group = ProductGroup.find(params[:id])
+    end
+
+    def product_group_params
+      params.require(:product_group).permit(:company_id, :product_category_id, :name, :active)
+    end
+
 end
