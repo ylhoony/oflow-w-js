d9740185fb6e891bcbd9d053df1f7344576ad761
Hoon Lee <yl.hoony@gmail.com>
2018-03-22 16:17:37 -0700

    added name_with_category method

--- app/models/product_group.rb
+++ app/models/product_group.rb
@@ -3,4 +3,8 @@ class ProductGroup < ApplicationRecord
   has_many :produts
 
   validates :name, presence: true, uniqueness: { scope: :product_category_id }
+
+  def name_with_category
+    "#{product_category.name} : #{name}"
+  end
 end
