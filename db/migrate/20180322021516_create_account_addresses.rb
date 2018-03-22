ed69e8d4ed49381819d55f1d6f9a27c9b1cc7e78
Hoon Lee <yl.hoony@gmail.com>
2018-03-21 19:38:42 -0700

    migration for account address

--- db/migrate/20180322021516_create_account_addresses.rb
+++ db/migrate/20180322021516_create_account_addresses.rb
@@ -1,9 +1,22 @@
 class CreateAccountAddresses < ActiveRecord::Migration[5.1]
   def change
     create_table :account_addresses do |t|
+      t.integer :account_id
       t.string :company_name
+      t.string :attention
+      t.string :address_line_1
+      t.string :address_line_2
+      t.string :city
+      t.string :state
+      t.integer :country_id
+      t.string :postal_code
+      t.string :email
+      t.string :phone
+      t.string :fax
+      t.text :comment
+      t.boolean :active, default: false
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180321172319) do
+ActiveRecord::Schema.define(version: 20180322021516) do
+
+  create_table "account_addresses", force: :cascade do |t|
+    t.integer "account_id"
+    t.string "company_name"
+    t.string "attention"
+    t.string "address_line_1"
+    t.string "address_line_2"
+    t.string "city"
+    t.string "state"
+    t.integer "country_id"
+    t.string "postal_code"
+    t.string "email"
+    t.string "phone"
+    t.string "fax"
+    t.text "comment"
+    t.boolean "active", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "accounts", force: :cascade do |t|
     t.string "type"
