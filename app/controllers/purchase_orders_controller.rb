3f8becfea9c6a747b42866732c76c6744028ada1
Hoon Lee <yl.hoony@gmail.com>
2018-03-23 14:28:30 -0700

    copied logic from sales_order and updated logic

--- app/controllers/purchase_orders_controller.rb
+++ app/controllers/purchase_orders_controller.rb
@@ -1,7 +1,60 @@
 class PurchaseOrdersController < ApplicationController
-
+  before_action :require_signed_in?
+  before_action :set_purchase_order, only: [:show, :edit, :update, :destroy]
+  
   def index
+    @purchase_orders = current_company.purchase_orders.all
+  end
+
+  def new
+    @purchase_order = PurchaseOrder.new
+  end
+
+  def create
+    @purchase_order = PurchaseOrder.new(purchase_order_params)
+    if @purchase_order.save
+      redirect_to purchase_orders_path
+    else
+      render :new
+    end
+  end
+
+  def show
     
   end
-  
+
+  def edit
+    
+  end
+
+  def update
+    if @purchase_order.update(purchase_order_params)
+      redirect_to purchase_orders_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @purchase_order.delete
+    redirect_to purchase_orders_path
+  end
+
+  private
+
+    def set_purchase_order
+      @purchase_order = PurchaseOrder.find(params[:id])
+    end
+
+    def purchase_order_params
+      params.require(:purchase_order).permit(
+        :company_address_id,
+        :warehouse_id,
+        :account_id,
+        :account_address_id,
+        :reference,
+        :comment,
+        :order_status_id)
+    end
+
 end
