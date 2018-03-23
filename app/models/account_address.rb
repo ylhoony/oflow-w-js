741d2687e1bbce5fd5f82c99986e75f80b7425cc
Hoon Lee <yl.hoony@gmail.com>
2018-03-23 14:07:04 -0700

    added name_with_account method

--- app/models/account_address.rb
+++ app/models/account_address.rb
@@ -2,4 +2,8 @@ class AccountAddress < ApplicationRecord
   belongs_to :account
   belongs_to :country
   has_many :sale_orders
+
+  def name_with_account
+    "#{account.name} : #{company_name}"
+  end
 end
