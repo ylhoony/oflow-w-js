8ea004f7f569c93af186f781fa6aeed5ce6c5a84
Hoon Lee <yl.hoony@gmail.com>
2018-03-24 16:50:00 -0700

    added tags_attributes=() method

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,8 +4,9 @@ class Product < ApplicationRecord
   belongs_to :unit_of_measure
   has_many :account_order_lines
   has_many :inventories
-  has_many :products_tags
-  has_many :tags, through: :products_tags
+  has_many :product_tags
+  has_many :tags, through: :product_tags
+  accepts_nested_attributes_for :tags
 
   validates :name, presence: true
   validates :sku, presence: true, uniqueness: true
@@ -14,4 +15,19 @@ class Product < ApplicationRecord
   def sku_with_name
     "#{sku} : #{name}"
   end
+
+  def tags_attributes=(tag_attributes)
+    tag_attributes.values.each do |tag_attribute|
+      if tag_attribute[:name].present?
+        tag = company.tags.find_by(tag_attribute)
+        if tag.present?
+          self.tags << tag
+        else
+          new_tag = company.tags.create(tag_attribute)
+          self.tags << new_tag
+        end
+      end
+    end
+  end
+
 end
