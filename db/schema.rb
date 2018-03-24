8d90ee93e2a5c1b079ecfa4ee7ff48b0a82b7641
Hoon Lee <yl.hoony@gmail.com>
2018-03-23 20:28:59 -0700

    migrated Tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180323220811) do
+ActiveRecord::Schema.define(version: 20180324032738) do
 
   create_table "account_addresses", force: :cascade do |t|
     t.integer "account_id"
@@ -204,6 +204,12 @@ ActiveRecord::Schema.define(version: 20180323220811) do
     t.string "name"
     t.decimal "price"
     t.integer "unit_of_measure_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
