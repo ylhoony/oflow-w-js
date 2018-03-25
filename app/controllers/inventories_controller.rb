1bc73e9a1e7cc50a948a4fc3d52c00dbdc7c72ae
Hoon Lee <yl.hoony@gmail.com>
2018-03-24 20:10:44 -0700

    added filter to inventories page

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,7 +1,10 @@
 class InventoriesController < ApplicationController
   before_action :require_signed_in?
 
+  helper_method :params
+
   def index
+    # binding.pry
     @inventories = current_company.inventories.all
   end
 end
