6d69bf8b2943832666f6a7e4f9601c3f7f948fce
Hoon Lee <yl.hoony@gmail.com>
2018-03-24 18:14:33 -0700

    added self.from_omniauth(auth) class method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  devise :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_many :employees
   has_many :companies, through: :employees
@@ -15,8 +16,13 @@ class User < ApplicationRecord
     Company.find(self.current_company_id) unless current_company_id.nil?
   end
 
-  # def self.without_current_employee
-  # DO THIS FOR EMPLOYEE FORM TO SHOW ONLY NON-EMPLOYEE USERS
-  # end
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 
 end
