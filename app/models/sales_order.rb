a11beb457564c0ab8d13c4a858410071e9e23e7f
Hoon Lee <yl.hoony@gmail.com>
2018-03-26 19:32:04 -0700

    set the query logic for sales_by_products

--- app/models/sales_order.rb
+++ app/models/sales_order.rb
@@ -22,4 +22,23 @@ class SalesOrder < AccountOrder
     end
   end
 
+  def self.so(current_company)
+    current_company.sales_orders
+  end
+
+  def self.sales_by_product(current_company)
+    self.so(current_company)
+      .joins(account_order_lines: :product)
+      .group("products.sku")
+      .order("total_sales DESC")
+      .select("products.sku, products.name, SUM(account_order_lines.quantity) AS total_quantity, SUM(account_order_lines.unit_price* account_order_lines.quantity) AS total_sales")
+  end
+
+  # Same Result testing in SQL statement:
+  # SELECT products.sku, SUM(account_order_lines.unit_price * account_order_lines.quantity) FROM account_orders
+  # INNER JOIN account_order_lines on account_orders.id = account_order_lines.account_order_id
+  # INNER JOIN products on account_order_lines.product_id = products.id
+  # where account_orders.type = "SalesOrder"
+  # GROUP BY products.sku;
+
 end
