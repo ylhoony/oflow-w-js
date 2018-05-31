99491eaa9c4601a4734f3996e3bfa9975543a847
Hoon Lee <yl.hoony@gmail.com>
2018-05-31 14:26:21 -0700

    implemented tags page

--- app/assets/javascripts/tags.js
+++ app/assets/javascripts/tags.js
@@ -1,3 +1,5 @@
+"use strict";
+
 class Tag {
   constructor(attributes) {
     this.id = attributes.id;
@@ -5,37 +7,59 @@ class Tag {
   }
 }
 
-$(() => {
-  // console.log("test");
-  init();
+$(document).on('turbolinks:load', () => {
+  const pathname = window.location.pathname;
+  if (pathname.includes("tags")) {
+    initTags();
+  }
+
 });
 
-const init = () => {
+const initTags = () => {
+  const pathname = window.location.pathname;
+  if (pathname.endsWith("/tags") || pathname.endsWith("/tags/")) {
+    initTagIndex();
+    tagsEventListener();
+  }
+}
+
+const initTagIndex = () => {
   ajaxData("get", "/tags", {})
     .done((res) => {
-      console.log(res);
+      const data = new Object;
+      data.tags = res.map(x => new Tag(x));
+      const tagsListSource = document.getElementById("tags-list-template").innerHTML;
+      const tagsListTemplate = Handlebars.compile(tagsListSource);
+      $("#tags-list").html(tagsListTemplate(data));
     })
     .fail((err) => {
       console.log(err);
-    });
-
-  // fetch("/tags", {
-  //   method: "get",
-  //   headers: {
-  //     'content-type': 'application/json'
-  //   }
-  // })
-  //   .then((res) => {
-  //     console.log(res);
-  //   })
+    })
 }
 
-const ajaxData = (method, dataURL, params) => {
-  return $.ajax({
-    method: method,
-    url: dataURL,
-    data: params,
-    dataType: "json"
+const tagsEventListener = () => {
+  $(".new_tag").on("click", (e) => {
+    e.preventDefault();
+    const tagFormSource = document.getElementById("tag-form-template").innerHTML;
+    const tagFormTemplate = Handlebars.compile(tagFormSource);
+    $("#tag-header").html(tagFormTemplate());
+
+    $("form#new_tag").on("submit", (e) => {
+      e.preventDefault();
+      const $form = $(e.target);
+      const method = $form.attr("method");
+      const action = $form.attr("action");
+      const params = $form.serialize();
+
+      ajaxData(method, action, params)
+        .done((res) => {
+          console.log(res);
+          $("#tag-header").empty();
+          initTags();
+        })
+        .fail((err) => {
+          console.log(err);
+        })
+    })
   });
 }
-
