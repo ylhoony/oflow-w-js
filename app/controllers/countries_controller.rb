f43346830f84c258d204553c54e98fc830b36378
Hoon Lee <yl.hoony@gmail.com>
2018-05-31 16:02:21 -0700

    updated index route with respond_to

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -4,6 +4,10 @@ class CountriesController < ApplicationController
 
   def index
     @countries = Country.all
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @countries, status: 201 }
+    end
   end
 
   def new
