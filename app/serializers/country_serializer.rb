c76fe314baa742bf13243c05ab17924cd26c3f59
Hoon Lee <yl.hoony@gmail.com>
2018-05-31 16:01:06 -0700

    opened name field

--- app/serializers/country_serializer.rb
+++ app/serializers/country_serializer.rb
@@ -1,6 +1,6 @@
 class CountrySerializer < ActiveModel::Serializer
-  has_many :company
+  has_many :companies
   has_many :warehouses
   
-  attributes :id, :alpha_2_code, :alpha_3_code, :numeric_code, :active
+  attributes :id, :name, :alpha_2_code, :alpha_3_code, :numeric_code, :active
 end
