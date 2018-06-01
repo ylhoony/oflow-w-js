99610034f46489e38f8fa89e6d5d417ecd9f1389
Hoon Lee <yl.hoony@gmail.com>
2018-05-31 17:11:11 -0700

    updated associations

--- app/serializers/product_category_serializer.rb
+++ app/serializers/product_category_serializer.rb
@@ -1,5 +1,6 @@
 class ProductCategorySerializer < ActiveModel::Serializer
   belongs_to :company
   has_many :product_groups
+  has_many :products
   attributes :id, :name, :active
 end
--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,8 +1,8 @@
 class ProductSerializer < ActiveModel::Serializer
   belongs_to :company
+  # belongs_to :product_category
   belongs_to :product_group
   belongs_to :unit_of_measure
-  # has_many :product_tags
   has_many :tags
 
   attributes :id, :sku, :name, :price
